1754527401583b1626b6b7efb5d7ec16955be75d
gabulyan_t_qschool <tigran.gabulyan.2001@mail.ru>
2024-08-01 11:28:46 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+import Home from "@/pages/Home/model/Home";
+
+export default Home;
---
+++ initFirebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBc3QqyCMr9f3qs8JAXnD1Y9XVKRwHhTbE",
+  authDomain: "messapp-8196c.firebaseapp.com",
+  projectId: "messapp-8196c",
+  storageBucket: "messapp-8196c.appspot.com",
+  messagingSenderId: "623695759792",
+  appId: "1:623695759792:web:2be2766b2c922d0ea42e75",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
